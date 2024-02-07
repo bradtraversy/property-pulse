10c7877093df268a7e982ef79d9600b547509860
Brad Traversy <traversymedia@gmail.com>
2024-02-07 12:17:53 -0500

    Created layout, homepage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+export default MainLayout;
---
+++ app/page.jsx
@@ -0,0 +1,4 @@
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+export default HomePage;
