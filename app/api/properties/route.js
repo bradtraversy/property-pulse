b22f81a244de88482f98e6d5668cf332187dba7f
Brad Traversy <traversymedia@gmail.com>
2024-02-11 12:58:38 -0500

    Add property form API route

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -16,3 +16,51 @@ export const GET = async (request) => {
     return new Response('Something Went Wrong', { status: 500 });
   }
 };
+
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData();
+
+    // Access all values from amenities and images
+    const amenities = formData.getAll('amenities');
+    const images = formData
+      .getAll('images')
+      .filter((image) => image.name !== '');
+
+    // Create propertyData object for database
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+        nightly: formData.get('rates.nightly.'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      images,
+    };
+
+    console.log(propertyData);
+
+    return new Response(JSON.stringify({ message: 'Success' }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response('Failed to add property', { status: 500 });
+  }
+};
--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -101,7 +101,11 @@ const PropertyAddForm = () => {
 
   return (
     mounted && (
-      <form>
+      <form
+        action='/api/properties'
+        method='POST'
+        encType='multipart/form-data'
+      >
         <h2 className='text-3xl text-center font-semibold mb-6'>
           Add Property
         </h2>
@@ -505,7 +509,7 @@ const PropertyAddForm = () => {
           <input
             type='text'
             id='seller_name'
-            name='seller_info.name.'
+            name='seller_info.name'
             className='border rounded w-full py-2 px-3'
             placeholder='Name'
             value={fields.seller_info.name}
@@ -563,6 +567,7 @@ const PropertyAddForm = () => {
             accept='image/*'
             multiple
             onChange={handleImageChange}
+            required
           />
         </div>
 
