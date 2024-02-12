c5ab16d5ff454b3e1a3ca2fa698cc5514d02896c
Brad Traversy <traversymedia@gmail.com>
2024-02-12 16:16:08 -0500

    Update property API route

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -55,3 +55,74 @@ export const DELETE = async (request, { params }) => {
     return new Response('Something Went Wrong', { status: 500 });
   }
 };
+
+// PUT /api/properties/:id
+export const PUT = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is required', { status: 401 });
+    }
+
+    const { id } = params;
+    const { userId } = sessionUser;
+
+    const formData = await request.formData();
+
+    // Access all values from amenities
+    const amenities = formData.getAll('amenities');
+
+    // Get property to update
+    const existingProperty = await Property.findById(id);
+
+    if (!existingProperty) {
+      return new Response('Property does not exist', { status: 404 });
+    }
+
+    // Verify ownership
+    if (existingProperty.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    // Create propertyData object for database
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+        nightly: formData.get('rates.nightly.'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      owner: userId,
+    };
+
+    // Update property in database
+    const updatedProperty = await Property.findByIdAndUpdate(id, propertyData);
+
+    return new Response(JSON.stringify(updatedProperty), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Failed to add property', { status: 500 });
+  }
+};
--- components/PropertyEditForm.jsx
+++ components/PropertyEditForm.jsx
@@ -113,7 +113,29 @@ const PropertyEditForm = () => {
     }));
   };
 
-  const handleSubmit = async () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData(e.target);
+
+      const res = await fetch(`/api/properties/${id}`, {
+        method: 'PUT',
+        body: formData,
+      });
+
+      if (res.status === 200) {
+        router.push(`/properties/${id}`);
+      } else if (res.status === 401 || res.status === 403) {
+        toast.error('Permission denied');
+      } else {
+        toast.error('Something went wrong');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    }
+  };
 
   return (
     mounted &&
