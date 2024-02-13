0ca64e671d86d92123e2f28842a1ea1fde53cf9a
Brad Traversy <traversymedia@gmail.com>
2024-02-13 10:16:43 -0500

    Bookmark button request

---
+++ app/api/bookmarks/route.js
@@ -0,0 +1,51 @@
+import connectDB from '@/config/database';
+import User from '@/models/User';
+import Property from '@/models/Property';
+import { getSessionUser } from '@/utils/getSessionUser';
+
+export const dynamic = 'force-dynamic';
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const { propertyId } = await request.json();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is required', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // Check if property is bookmarked
+    let isBookmarked = user.bookmarks.includes(propertyId);
+
+    let message;
+
+    if (isBookmarked) {
+      // If already bookmarked, remove it
+      user.bookmarks.pull(propertyId);
+      message = 'Bookmark removed successfully';
+      isBookmarked = false;
+    } else {
+      // If not bookmarked, add it
+      user.bookmarks.push(propertyId);
+      message = 'Bookmark added successfully';
+      isBookmarked = true;
+    }
+
+    await user.save();
+
+    return new Response(JSON.stringify({ message, isBookmarked }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,8 +1,48 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { toast } from 'react-toastify';
 import { FaBookmark } from 'react-icons/fa';
 
-const BookmarkButton = () => {
+const BookmarkButton = ({ property }) => {
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const handleClick = async () => {
+    if (!userId) {
+      toast.error('You need to sign in to bookmark a property');
+      return;
+    }
+
+    try {
+      const res = await fetch('/api/bookmarks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          propertyId: property._id,
+        }),
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        toast.success(data.message);
+        setIsBookmarked(data.isBookmarked);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    }
+  };
+
   return (
-    <button className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
+    <button
+      onClick={handleClick}
+      className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'
+    >
       <FaBookmark className='mr-2' /> Bookmark Property
     </button>
   );
