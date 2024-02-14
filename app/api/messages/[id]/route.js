d0336c3df2c575d271c0b27909f84561b13a493c
Brad Traversy <traversymedia@gmail.com>
2024-02-14 18:43:42 -0500

    Mark messages as read

---
+++ app/api/messages/[id]/route.js
@@ -0,0 +1,43 @@
+import connectDB from '@/config/database';
+import Message from '@/models/Message';
+import { getSessionUser } from '@/utils/getSessionUser';
+
+export const dynamic = 'force-dynamic';
+
+// PUT /api/messages/:id
+export const PUT = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const { id } = params;
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response('User ID is required', {
+        status: 401,
+      });
+    }
+
+    const { userId } = sessionUser;
+
+    const message = await Message.findById(id);
+
+    if (!message) return new Response('Message Not Found', { status: 404 });
+
+    // Verify ownership
+    if (message.recipient.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    // Update message to read/unread depending on the current status
+    message.read = !message.read;
+
+    await message.save();
+
+    return new Response(JSON.stringify(message), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
--- components/Message.jsx
+++ components/Message.jsx
@@ -1,6 +1,38 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
 const Message = ({ message }) => {
+  const [isRead, setIsRead] = useState(message.read);
+
+  const handleReadClick = async () => {
+    try {
+      const res = await fetch(`/api/messages/${message._id}`, {
+        method: 'PUT',
+      });
+
+      if (res.status === 200) {
+        const { read } = await res.json();
+        setIsRead(read);
+        if (read) {
+          toast.success('Marked as read');
+        } else {
+          toast.success('Marked as new');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    }
+  };
+
   return (
     <div className='relative bg-white p-4 rounded-md shadow-md border border-gray-200'>
+      {!isRead && (
+        <div className='absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md'>
+          New
+        </div>
+      )}
       <h2 className='text-xl mb-4'>
         <span className='font-bold'>Property Inquiry:</span>{' '}
         {message.property.name}
@@ -29,8 +61,13 @@ const Message = ({ message }) => {
           {new Date(message.createdAt).toLocaleString()}
         </li>
       </ul>
-      <button className='mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md'>
-        Mark As Read
+      <button
+        onClick={handleReadClick}
+        className={`mt-4 mr-3 ${
+          isRead ? 'bg-gray-300' : 'bg-blue-500 text-white'
+        } py-1 px-3 rounded-md`}
+      >
+        {isRead ? 'Mark As New' : 'Mark As Read'}
       </button>
       <button className='mt-4 bg-red-500 text-white py-1 px-3 rounded-md'>
         Delete
