8ef2cb46f2a32e4f6988be83829655a35b57a9ed
Brad Traversy <traversymedia@gmail.com>
2024-02-15 13:23:29 -0500

    Photoswipe lightbox

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import { ToastContainer } from 'react-toastify';
 import { GlobalProvider } from '@/context/GlobalContext';
 import '@/assets/styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -1,46 +1,71 @@
 import Image from 'next/image';
+import { Gallery, Item } from 'react-photoswipe-gallery';
 
 const PropertyImages = ({ images }) => {
   return (
-    <section className='bg-blue-50 p-4'>
-      <div className='container mx-auto'>
-        {images.length === 1 ? (
-          <Image
-            src={images[0]}
-            alt=''
-            className='object-cover h-[400px] mx-auto rounded-xl'
-            width={1800}
-            height={400}
-            priority={true}
-          />
-        ) : (
-          <div className='grid grid-cols-2 gap-4'>
-            {images.map((image, index) => (
-              <div
-                key={index}
-                className={`
+    <Gallery>
+      <section className='bg-blue-50 p-4'>
+        <div className='container mx-auto'>
+          {images.length === 1 ? (
+            <Item
+              original={images[0]}
+              thumbnail={images[0]}
+              width='1000'
+              height='600'
+            >
+              {({ ref, open }) => (
+                <Image
+                  ref={ref}
+                  onClick={open}
+                  src={images[0]}
+                  alt=''
+                  className='object-cover h-[400px] mx-auto rounded-xl'
+                  width={1800}
+                  height={400}
+                  priority={true}
+                />
+              )}
+            </Item>
+          ) : (
+            <div className='grid grid-cols-2 gap-4'>
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className={`
                   ${
                     images.length === 3 && index === 2
                       ? 'col-span-2'
                       : 'col-span-1'
                   }
                 `}
-              >
-                <Image
-                  src={image}
-                  alt=''
-                  className='object-cover h-[400px] w-full rounded-xl'
-                  width={0}
-                  height={0}
-                  sizes='100vw'
-                  priority={true}
-                />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+                >
+                  <Item
+                    original={image}
+                    thumbnail={image}
+                    width='1000'
+                    height='600'
+                  >
+                    {({ ref, open }) => (
+                      <Image
+                        ref={ref}
+                        onClick={open}
+                        src={image}
+                        alt=''
+                        className='object-cover h-[400px] w-full rounded-xl'
+                        width={0}
+                        height={0}
+                        sizes='100vw'
+                        priority={true}
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </Gallery>
   );
 };
 export default PropertyImages;
